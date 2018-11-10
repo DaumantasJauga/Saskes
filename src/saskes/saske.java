98b47d5a8c04af025ad787827ef2d75321c5ad88
Daumantas <daumantas@jauga.lt>
2018-11-10 19:14:54 +0200

    first init

---
+++ src/saskes/saske.java
@@ -0,0 +1,31 @@
+package saskes;
+
+public class saske {
+	
+	int checkerX;
+	int checkerY;
+	
+	saske(int x, int y){
+		this.checkerX=x;
+		this.checkerY=y;
+	}
+
+	public int getCheckerX() {
+		return checkerX;
+	}
+
+	public void setCheckerX(int checkerX) {
+		this.checkerX = checkerX;
+	}
+
+	public int getCheckerY() {
+		return checkerY;
+	}
+
+	public void setCheckerY(int checkerY) {
+		this.checkerY = checkerY;
+	}
+	
+	
+
+}
